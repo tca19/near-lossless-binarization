a0004fbb6502ea93ed4a8e5b18452072fcf5ade3
tca19 <30314448+tca19@users.noreply.github.com>
2019-02-02 14:39:45 -1000

    Add GPL license header in spearman.c and utils.h

--- spearman.c
+++ spearman.c
@@ -1,3 +1,24 @@
+/* Copyright (c) 2019-present, All rights reserved.
+ * Written by Julien Tissier <30314448+tca19@users.noreply.github.com>
+ *
+ * This file is part of the "Near-lossless Binarization of Word Embeddings"
+ * software (https://github.com/tca19/near-lossless-binarization).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License at the root of this repository for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdlib.h>  /* malloc(), qsort(), free() */
 #include <string.h>  /* memcpy() */
 #include <math.h>    /* sqrt() */
