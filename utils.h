08a27081db1649e04b2a1a048a36611fe8cf2d8c
tca19 <30314448+tca19@users.noreply.github.com>
2020-07-14 19:05:17 +0200

    Move the function `binary_sim()` at the end of file_process.c

--- utils.h
+++ utils.h
@@ -30,9 +30,9 @@ void lower(char*);
 float spearman_coef(float*, float*, int);
 
 /* file_process.c */
-float binary_sim(const void*, const void*, const int);
 void create_vocab(const char*);
 unsigned long **load_binary_vectors(const char*, long*, int*, int*);
 float         **load_real_vectors(const char*, long *, int*);
 void evaluate(const char*, void**, int,
               float (*f)(const void*, const void*, const int));
+float binary_sim(const void*, const void*, const int);
